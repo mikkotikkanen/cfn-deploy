e92360597c623ccb88ccab54a8e11dbbc43aac9e
Mikko Tikkanen <mikko.tikkanen@gmail.com>
2018-11-01 19:43:52 +0200

    Added capability tests

---
+++ tests/create-stack/create-stack-capabilities.test.js
@@ -0,0 +1,23 @@
+const mockCreateStack = require('../mocks/createStack');
+const lib = require('../..');
+
+
+describe('cfn-deploy', () => {
+  beforeAll(() => mockCreateStack.beforeAll());
+
+  it('should successfully create new stack', (done) => {
+    const events = lib({
+      region: 'us-east-1',
+      stackName: 'new-stack',
+      template: './tests/templates/simple-template.yaml',
+      capabilities: 'CAPABILITY_IAM',
+    });
+    events.on('ERROR', err => done(err));
+    events.on('COMPLETE', (data) => {
+      expect(data).toMatchSnapshot();
+    });
+    events.on('FINALLY', done);
+  });
+
+  afterAll(() => mockCreateStack.afterAll());
+});
---
+++ tests/update-stack/update-stack-capabilities.test.js
@@ -0,0 +1,23 @@
+const mockCreateStack = require('../mocks/updateStack');
+const lib = require('../..');
+
+
+describe('cfn-deploy', () => {
+  beforeAll(() => mockCreateStack.beforeAll());
+
+  it('should successfully update existing stack', (done) => {
+    const events = lib({
+      region: 'us-east-1',
+      stackName: 'existing-stack',
+      template: './tests/templates/simple-template.yaml',
+      capabilities: 'CAPABILITY_IAM',
+    });
+    events.on('ERROR', err => done(err));
+    events.on('COMPLETE', (data) => {
+      expect(data).toMatchSnapshot();
+    });
+    events.on('FINALLY', done);
+  });
+
+  afterAll(() => mockCreateStack.afterAll());
+});
