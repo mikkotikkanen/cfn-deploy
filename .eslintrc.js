e4190852d413e6c517fd2dbca39221f5ebc7e279
Mikko Tikkanen <mikko.tikkanen@gmail.com>
2019-05-14 22:47:46 +0300

    removed unused eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,16 @@
 module.exports = {
-    env: {
-      es6: true,
-      node: true,
-      browser: true,
-      jest: true,
-    },
-    extends: ['airbnb-base', 'plugin:node/recommended'],
-    rules: {
-      'no-console': 'off',
-    },
-    plugins: [
-    ],
-    parserOptions: {
-    },
-  }
+  env: {
+    es6: true,
+    node: true,
+    browser: true,
+    jest: true,
+  },
+  extends: ['airbnb-base'],
+  rules: {
+    'no-console': 'off',
+  },
+  plugins: [
+  ],
+  parserOptions: {
+  },
+}
